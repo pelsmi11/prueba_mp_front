d8a80b3a06b0139c2ff8b3c3a07e96b835704b81
hector Martinez <hmartinezmoreira81@gmail.com>
2024-09-03 23:02:07 -0600

    feat: mode vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Cargar las variables de entorno basadas en el modo
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      "process.env": env,
+    },
+  };
+});
