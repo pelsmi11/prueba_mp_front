b3174516df06bc7850e65a197c5dbf704df94598
hector Martinez <hmartinezmoreira81@gmail.com>
2024-09-03 23:07:37 -0600

    feat: test

--- src/api/config.js
+++ src/api/config.js
@@ -1,21 +1,23 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+  baseURL:
+    import.meta.env.VITE_API_URL ??
+    "https://prueba-mp-backend-834460423898.us-central1.run.app/api/v1/",
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const token = Cookies.get('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Cargar las variables de entorno basadas en el modo
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    define: {
-      "process.env": env,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
